42a4699893cb3d6f2b9217d3327dfc29280874f8
rafapro1 <raafhxs_spy@live.com>
2024-12-04 17:13:25 -0300

    function

---
+++ fubction/script.js
@@ -0,0 +1,34 @@
+/*function saudacao(nome) {
+return `Bom dia ${nome}!`
+
+}
+
+const variavel = saudacao('luiz')
+console.log(variavel);
+
+
+saudacao('Maria')
+saudacao('Luiz')
+saudacao('rafael')
+saudacao('felipe')
+saudacao('Jorge')
+
+function soma(x, y) {
+    const resultado = x + y;
+    return resultado;
+}
+
+
+const resultado = soma(2);
+console.log(resultado)
+
+const raiz = function(n) {
+    return n ** 0.5;
+};
+
+console.log(raiz(9));*/
+
+
+const raiz = n => n ** 0.5; //arrow function
+
+console.log(raiz(9))
