65c0108a589b34eebc86c7936e6e9ce8296eae1f
rafapro1 <raafhxs_spy@live.com>
2024-12-12 10:11:16 -0300

    date

---
+++ exercicios date/script.js
@@ -0,0 +1,13 @@
+const h1 = document.querySelector('h1');
+const data = new Date();
+
+h1.innerHTML = data.toString('pt-br');
+
+
+/*minhaDiv.addEventListener('console.log', function (e) {
+    e.preventDefault();
+    console.log('ola')
+
+});
+
+console.log(dataAtual);*/
